b8d8ec1a67e0826bc2c4d41ce8280d6ab74cd360
phuocnguyen <nphuocnguyen2000@gmail.com>
2020-07-27 15:37:23 +0700

    done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -122,7 +122,7 @@ module.exports.login = (req, res)=>{
                 res.json(user)
               }
               else{
-                res.json({errors: false, message: "Login failed"})
+                res.json({errors: true, message: "Login failed"})
               }
             });
             
